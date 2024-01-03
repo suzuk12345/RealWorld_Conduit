dc84d159f15f36947b0bb08c9556887ab9a538e8
Takafumi Suzuki <tatakaka.0301.ini@gmail.com>
2024-01-03 17:22:29 +0900

    .env.example修正

--- routes/web.php
+++ routes/web.php
@@ -26,24 +26,24 @@ Route::prefix('conduit')
 ->controller(ConduitArticleController::class)
 ->group(function () {
     Route::get('/', 'index')->name('index'); // ホーム
-    Route::get('/editor', 'editorNew')->name('editorNew'); // 記事新規作成
-    Route::post('/', 'store')->name('store'); // 記事投稿
-    Route::get('/editor/article{id}', 'editorExisting')->name('editorExisting'); // 記事編集
-    Route::post('/editor/article{id}', 'update')->name('update'); // 記事更新
-    Route::get('/article/article{id}', 'article')->name('article'); // 記事閲覧
-    Route::post('/article/article{id}/destroy', 'destroy')->name('destroy'); // 記事削除
+    Route::get('/editor', 'editorNew')->name('editorNew'); // 新規作成
+    Route::post('/', 'store')->name('store'); // 投稿
+    Route::get('/editor/article{id}', 'editorExisting')->name('editorExisting'); // 編集
+    Route::post('/editor/article{id}', 'update')->name('update'); // 更新
+    Route::get('/article/article{id}', 'article')->name('article'); // 閲覧
+    Route::post('/article/article{id}/destroy', 'destroy')->name('destroy'); // 削除
 });
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__ . '/auth.php';
